0a1cd5ec36ab0772408bd3d68c6fb3020019bc51
83320843 <yagguanglin2@gmail.com>
2019-09-08 19:58:55 +0800

    家

--- Chapter01.py
+++ Chapter01.py
@@ -24,3 +24,20 @@
 # print(homedir)
 # print(sh)
 
+# 你想解压一些元素后丢弃它们，你不能简单就使用 * ，但是你可以使用一
+# 个普通的废弃名称，比如 _ 或者 ign
+# record = ('ACME', 50, 123,45, (12,18,2012))
+# name, *_, (*_, year) = record
+# print(name)
+# print(year)
+
+# 使用星号进行递归算法
+# items = [1, 10, 7, 4, 5, 9]
+# head, *tail = items
+# print(head)
+# print(tail)
+
+# def sum(items):
+#     head, *tail = items
+#     return head + sum(tail) if tail else head
+# print(sum(items))
